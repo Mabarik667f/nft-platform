51006a9c338eddee2bdcd58293b3befe931cd9c7
Mabarik667f <jc.bob1412@gmail.com>
2024-03-03 01:18:13 +0300

    delete users app

--- src/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import UserLoginView, UserProfileView
-
-app_name = 'users'
-urlpatterns = [
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('profile/', UserProfileView.as_view(), name='profile')
-]
