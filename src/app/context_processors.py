2c7b39dee689119914b4f7c3f5ddd92df89a491c
Mabarik667f <jc.bob1412@gmail.com>
2024-03-04 12:58:53 +0300

    add new templates, fix templates and styles

--- src/app/context_processors.py
+++ src/app/context_processors.py
@@ -1,7 +1,7 @@
 main_menu = [{'url': 'home', 'title': 'Главная'},
              {'url': 'auctions', 'title': 'Аукционы'},
              {'url': 'nft-market', 'title': 'NFT'},
-             {'url': 'users:profile', 'title': 'Профиль'},]
+             {'url': 'profile', 'title': 'Профиль'},]
 
 profile_menu = [{'url': 'show-nft', 'title': 'Одиночные NFT'},
                  {'url': 'show-collections', 'title': 'Коллекции'}]
